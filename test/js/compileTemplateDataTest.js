4526ab84a6e613b16b03afe1d3c730b1c316f97c
Antonio Greco <agreco13@gmail.com>
2014-07-06 23:16:37 +0100

    Adding jsdom in compileTemplateDataTest, enabling the parsing of default template structure with jquery

--- test/js/compileTemplateDataTest.js
+++ test/js/compileTemplateDataTest.js
@@ -1,10 +1,12 @@
 var _ = require('lodash'),
-    path = require('path'),
-    fs = require('fs'),
-    dox = require('dox').parseComments,
-    helpers = require('../../src/helpers'),
     chai = require('chai'),
+    dox = require('dox').parseComments,
     expect = chai.expect,
+    fs = require('fs'),
+    helpers = require('../../src/helpers'),
+    jquery = fs.readFileSync('template/default/js/lib/jquery.min.js', 'utf-8'),
+    jsdom = require('jsdom'),
+    path = require('path'),
     should = chai.should();
 
 describe('.compileTemplateData()', function () {
@@ -16,7 +18,18 @@ describe('.compileTemplateData()', function () {
 
     it('should create the default template from a given document', function () {
         var files = helpers.buildFileObjects(helpers.cleanseFiles(helpers.getFiles('./test/resources/'))),
-            rendered, template = fs.readFileSync(path.resolve('template/default/index.html'), "utf8").toString();
-        _.each(files ? files : [], function (file) { rendered = helpers.compileTemplateData(file, template); });
+            renderedTmpl, template = fs.readFileSync(path.resolve('template/default/index.html'), "utf8").toString();
+        _.each(files ? files : [], function (file) { renderedTmpl = helpers.compileTemplateData(file, template); });
+
+        return expect(renderedTmpl).to.not.be.empty, jsdom.env({
+            html: renderedTmpl,
+            src: [jquery],
+            done: function (err, window) {
+                var $ = window.$, content = $('#content');
+                return expect(content.find('h1').text()).to.not.be.empty,
+                       expect(content.find('p').text()).to.not.be.empty,
+                       expect(content.find('#api').text()).to.not.be.empty;
+            }
+        });
     });
 });
